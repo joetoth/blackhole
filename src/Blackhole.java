e37ea8024b87a3806c85b5ac95435883e4f73b55
joetoth <joetoth@gmail.com>
2018-07-25 21:58:57 -0400

    holey

--- src/Blackhole.java
+++ src/Blackhole.java
@@ -1,3 +1,4 @@
+import com.google.gson.Gson;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
@@ -6,34 +7,41 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Blackhole extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent event) {
         Project project = event.getData(PlatformDataKeys.PROJECT);
-//        new ActionPerformer(new GithubRepo(project.getBasePath())).actionPerformed(event);
         final Editor editor = event.getData(PlatformDataKeys.EDITOR);
         final VirtualFile file = event.getData(PlatformDataKeys.VIRTUAL_FILE);
 
-        if (editor.getSelectionModel().hasSelection()) {
+        if (!editor.getSelectionModel().hasSelection()) {
             editor.getSelectionModel().selectLineAtCaret();
         }
-
         String selected = editor.getSelectionModel().getSelectedText();
         editor.getSelectionModel().removeSelection();
 
-//        Integer line = (editor != null)
-//                // convert the VisualPosition to the LogicalPosition to have the correct line number.
-//                // http://grepcode.com/file/repository.grepcode.com/java/ext/com.jetbrains/intellij-idea/10.0/com/intellij/openapi/editor/LogicalPosition.java#LogicalPosition
-//                ? editor.visualToLogicalPosition(
-//                editor.getSelectionModel().getSelectionStartPosition()).line + 1 : null;
+        Integer lineno = (editor != null)
+                // convert the VisualPosition to the LogicalPosition to have the correct line number.
+                // http://grepcode.com/file/repository.grepcode.com/java/ext/com.jetbrains/intellij-idea/10.0/com/intellij/openapi/editor/LogicalPosition.java#LogicalPosition
+                ? editor.visualToLogicalPosition(
+                editor.getSelectionModel().getSelectionStartPosition()).line + 1 : null;
 
-        String context = "intellijj:32:filename.java";
-        String[] cmd = {"blackhole", context, selected};
+        Map json = new HashMap();
+        json.put("context", "intellij");
+        json.put("file_path", file.getPath());
+        json.put("project_path", project.getBaseDir().getPath());
+        json.put("lineno", lineno);
+        json.put("text", selected);
+        Gson g = new Gson();
+        String[] cmd = {"/usr/local/bin/blackhole", g.toJson(json)};
         try {
-            Process myProcess = Runtime.getRuntime().exec(cmd);
+            Runtime.getRuntime().exec(cmd);
         } catch(IOException e) {
             e.printStackTrace();
         }
     }
 }
+
